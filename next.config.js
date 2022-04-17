ba6582f6862026d44a909920edcb5af8a45fc6e7
collect 3500 <collect3500@gmail.com>
2022-04-17 18:19:04 -0400

    e

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	images: {
+		domains: ['media.discordapp.net'],
+	},
+	async redirects() {
+		return [
+			{
+				source: '/abs',
+				destination: 'https://www.roblox.com/games/7107498084',
+				permanent: false,
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
